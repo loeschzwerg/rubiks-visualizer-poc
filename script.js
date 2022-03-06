5092692db8ba964edbed1481291953af764400a4
loeschzwerg <tim.nebel@hhu.de>
2022-03-06 14:28:38 +0100

    format code

--- script.js
+++ script.js
@@ -22,31 +22,33 @@ var cube = {
     orange: side("orange")
 }
 
-const id = [0,1,2,3,4,5,6,7,8];
-const cw = [6,3,0,7,4,1,8,5,2];
-const dcw = [8,7,6,5,4,3,2,1,0];
-const ccw = [2,5,8,1,4,7,0,3,6]
+const id = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const cw = [6, 3, 0, 7, 4, 1, 8, 5, 2];
+const dcw = [8, 7, 6, 5, 4, 3, 2, 1, 0];
+const ccw = [2, 5, 8, 1, 4, 7, 0, 3, 6]
+
+
 
 function dir_str(direction) {
-    return direction==id ? "id" : direction==cw ? "cw" : direction==dcw ? "dcw" : "ccw"
+    return direction == id ? "id" : direction == cw ? "cw" : direction == dcw ? "dcw" : "ccw"
 }
 
-function rotate(direction, color){
+function rotate(direction, color) {
     console.log("rotate", color, dir_str(direction))
-    let copy = {...cube[color]}
+    let copy = { ...cube[color] }
     for (let i = 0; i < 9; i++) {
         cube[color][i] = copy[direction[i]];
         render_cube();
     }
 }
 
-function align_top(sides){
+function align_top(sides) {
     for (const side of sides) {
         rotate(side[0], side[1])
     }
 }
 
-function unalign_top(sides){
+function unalign_top(sides) {
     for (const side of sides) {
         switch (side[0]) {
             case cw: rotate(ccw, side[1]); break;
@@ -58,10 +60,10 @@ function unalign_top(sides){
 
 }
 
-function swap_top_row(above, right, below, left, clockwise){
-    let old = [above.slice(0,3), right.slice(0,3), below.slice(0,3), left.slice(0,3)];
+function swap_top_row(above, right, below, left, clockwise) {
+    let old = [above.slice(0, 3), right.slice(0, 3), below.slice(0, 3), left.slice(0, 3)];
     console.log("top row:", old)
-    if (clockwise){
+    if (clockwise) {
         for (let i = 0; i < 3; i++) {
             [above[i], right[i], below[i], left[i]] = [old[1][i], old[2][i], old[3][i], old[0][i]]
         }
@@ -74,11 +76,11 @@ function swap_top_row(above, right, below, left, clockwise){
 
 const turns = {
     yellow: [[dcw, "orange"], [id, "green"], [id, "red"], [id, "blue"]],
-    red:    [[dcw, "yellow"], [cw, "green"], [id, "white"], [ccw, "blue"]],
-    white:  [[dcw, "red"], [dcw, "green"], [id, "orange"], [dcw, "blue"]],
+    red: [[dcw, "yellow"], [cw, "green"], [id, "white"], [ccw, "blue"]],
+    white: [[dcw, "red"], [dcw, "green"], [id, "orange"], [dcw, "blue"]],
     orange: [[dcw, "white"], [ccw, "green"], [id, "yellow"], [cw, "blue"]],
-    blue:   [[cw, "yellow"], [cw, "red"], [cw, "white"], [cw, "orange"]],
-    green:  [[ccw, "yellow"], [ccw, "orange"], [ccw, "white"], [ccw, "red"]]
+    blue: [[cw, "yellow"], [cw, "red"], [cw, "white"], [cw, "orange"]],
+    green: [[ccw, "yellow"], [ccw, "orange"], [ccw, "white"], [ccw, "red"]]
 }
 
 function turn_face(color, clockwise) {
@@ -89,7 +91,7 @@ function turn_face(color, clockwise) {
     unalign_top([above, right, below, left])
 }
 
-function turn_cube(color, clockwise){
+function turn_cube(color, clockwise) {
     console.log("-> turn", color, clockwise ? "clockwise" : "counterclockwise");
     turn_face(color, clockwise);
     render_cube();
